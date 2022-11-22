5a8d1cd86e2bc3ef0ee7bae8b4c40424581c44c2
Matej Drobnič <matej@matejdro.com>
2022-11-22 12:06:44 +0100

    feat: add async and static list of image tuples

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+use std::fmt::{Debug, Error, Formatter};
+
+struct Image  {
+    url: String,
+    width: i32,
+    height: i32,
+}
+
+impl Debug for Image {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.debug_tuple("")
+            .field(&self.url)
+            .field(&self.width)
+            .field(&self.height)
+            .finish()
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let images = load_images_from_reddit().await?;
+
+    println!("Got images: {:?}", images);
+    return Ok(());
+}
+
+async fn load_images_from_reddit() -> Result<Vec<Image>, Error> {
+    return Ok(vec![
+        Image {
+            url: "images.com/image1.png".to_string(),
+            width: 1280,
+            height: 720
+        },
+        Image {
+            url: "images.com/image2.png".to_string(),
+            width: 1920,
+            height: 1080
+        }
+    ])
+    // return Ok(Vec::new())
 }
